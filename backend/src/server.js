585e3f9b60588b376695885ca8f84c1ab48c8bf5
Radsonb <radsonbrendon02@gmail.com>
2025-07-12 15:17:43 -0300

    feat: edit serverjs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const connectDB = require("../database/config/database");
+const routes = require('./routes');
 require("dotenv").config();
 
 connectDB();
-
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes);
 
 app.get('/api/test', (req, res) => {
   res.json({ message: "Rota de teste funcionando!" });
